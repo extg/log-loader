62ece8126817317df1cb92c7f9020aa40bcec46d
Vasily Krylov <tor@outsorcerers.com>
2017-02-18 14:55:58 +0300

    Forgot add file under git

---
+++ get-params.js
@@ -0,0 +1,33 @@
+
+
+var path = require('path');
+var loader = require('loader-utils');
+
+module.exports = getParams;
+
+/**
+ * Extraction params of loader
+ * @param loaderContext
+ * @returns {Object}
+ */
+function getParams(loaderContext) {
+    var query = loader.parseQuery(loaderContext.query);
+    var options = loader.getLoaderConfig(loaderContext);
+    var params = Object.assign(query, options);
+    var filePath = path.relative(loaderContext.options.context, loaderContext.resourcePath);
+
+    return {
+        id: params.id ? `[${params.id}]` : '',
+        file: `./${filePath}`,
+        showSource: Boolean(params.showSource),
+        showPitch: Boolean(params.showPitch),
+        showPrevLoader: params.showPrevLoader,
+        showNextLoader: params.showNextLoader,
+        showIndex: params.showIndex,
+        callback: options.callback,
+        callbackPitch: options.callbackPitch,
+        pad: params.pad || {},
+        options: options,
+        query: query
+    }
+}
